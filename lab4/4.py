24b3d1fccd54240aaf97786fecc919a89cee8fb8
choockleet-sys <choockleet@gmail.com>
2026-05-05 10:58:33 +0500

    gfsd

--- lab4/4.py
+++ lab4/4.py
@@ -1,11 +1,15 @@
 from functools import wraps
-def check(func: callable)-> callable:
-    @wraps(func)
-    def wraper(*args,**kwargs):
-        if isinstance(*args,(int,float)):
-            r = func(*args,**kwargs)
-            return r
-    return wraper
+def check(types= None, mi_v = None, ma_v=None):
+    def chec_d(func: callable)-> callable:
+        @wraps(func)
+        def wraper(*args,**kwargs):
+            if types is not None:
+                for arg in args:
+                    if not isinstance(arg,types):
+                       raise TypeError(f" Аргумент {arg} должен быть {types}, а не {type(arg)}")
+            return func(*args,**kwargs)
+        return wraper
+    return chec_d
 def summa(*args):
     def wraper(*args):
         s=0
@@ -20,3 +24,4 @@ def summa(*args):
         su+=i
     return  wraper(*args)
 summa(1,34,432,543,324,1324)
+check(types = (int, float),mi_v=0, ma_v=600)(summa(1,34,432,543,324,1324,"hp"))
