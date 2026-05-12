8a1d40e2115399f49feed8275f315e386ef38b23
choockleet-sys <choockleet@gmail.com>
2026-05-12 10:13:01 +0500

    Перевёл ошибки

--- lab4/4.py
+++ lab4/4.py
@@ -6,15 +6,15 @@ def check(types= None, mi_v = None, ma_v=None):
             if types is not None:
                 for arg in args:
                     if not isinstance(arg,types):
-                       raise TypeError(f"Argument {arg} must be of type {types}, got {type(arg)}")
+                       raise TypeError(f"Аргумент {arg} должен быть {types}, а не {type(arg)}")
             if mi_v is not None or ma_v is not None:
                 for arg in args:
                     if not isinstance(arg, (int, float)):
                         continue
                     if mi_v is not None and arg < mi_v:
-                        raise ValueError(f"Argument {arg} is less than {mi_v}")
+                        raise ValueError(f"Аргумент {arg} меньше чем {mi_v}")
                     if ma_v is not None and arg > ma_v:
-                        raise ValueError(f"Argument {arg} is greater than {ma_v}")
+                        raise ValueError(f"Аргумент {arg} больше чем {ma_v}")
             return func(*args, **kwargs)
         return wraper
     return chec_d
@@ -40,4 +40,4 @@ summa(1,34,432,543,324,124)
 a = summa()
 a(7,8,8)
 a(5,3,65,43)
-#a(45,23,342,342432)
+#a(45,23,342,6434)
